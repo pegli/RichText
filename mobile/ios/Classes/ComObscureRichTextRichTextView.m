111f715fd9d0b8cc0362b43a0f8dfbea88ce90d8
Mads Møller <info@viezel.dk>
2013-04-06 16:44:54 +0200

    Updated DTCoreText to v1.4

--- mobile/ios/Classes/ComObscureRichTextRichTextView.m
+++ mobile/ios/Classes/ComObscureRichTextRichTextView.m
@@ -7,7 +7,12 @@
 //
 
 #import "ComObscureRichTextRichTextView.h"
+#import <QuartzCore/QuartzCore.h>
+#import <MediaPlayer/MediaPlayer.h>
+
 #import "TiViewProxy.h"
+#import "DTVersion.h"
+#import "DTTiledLayerWithoutFade.h"
 
 // as per http://www.cocoanetics.com/2011/08/nsattributedstringhtml-qa/
 #define kDefaultFontSize 12.0 
@@ -121,12 +126,204 @@
     [self setAttributedTextViewContent];
 }
 
-// TODO setMarkdown_ ?
 
 #pragma mark -
 #pragma mark DTAttributedTextContentViewDelegate
 
-- (void)linkPushed:(DTLinkButton *)button {
+- (UIView *)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView viewForAttributedString:(NSAttributedString *)string frame:(CGRect)frame
+{
+	NSDictionary *attributes = [string attributesAtIndex:0 effectiveRange:NULL];
+	
+	NSURL *URL = [attributes objectForKey:DTLinkAttribute];
+	NSString *identifier = [attributes objectForKey:DTGUIDAttribute];
+	
+	
+	DTLinkButton *button = [[DTLinkButton alloc] initWithFrame:frame];
+	button.URL = URL;
+	button.minimumHitSize = CGSizeMake(25, 25); // adjusts it's bounds so that button is always large enough
+	button.GUID = identifier;
+	
+	// get image with normal link text
+	UIImage *normalImage = [attributedTextContentView contentImageWithBounds:frame options:DTCoreTextLayoutFrameDrawingDefault];
+	[button setImage:normalImage forState:UIControlStateNormal];
+	
+	// get image for highlighted link text
+	UIImage *highlightImage = [attributedTextContentView contentImageWithBounds:frame options:DTCoreTextLayoutFrameDrawingDrawLinksHighlighted];
+	[button setImage:highlightImage forState:UIControlStateHighlighted];
+	
+	// use normal push action for opening URL
+	[button addTarget:self action:@selector(linkPushed:) forControlEvents:UIControlEventTouchUpInside];
+	
+	// demonstrate combination with long press
+	UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(linkLongPressed:)];
+	[button addGestureRecognizer:longPress];
+	
+	return button;
+}
+
+- (UIView *)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView viewForAttachment:(DTTextAttachment *)attachment frame:(CGRect)frame
+{
+	NSLog(@"contentType %@", attachment.contentType);
+    
+    if (attachment.contentType == DTTextAttachmentTypeVideoURL)
+	{
+		NSURL *url = (id)attachment.contentURL;
+		
+		// we could customize the view that shows before playback starts
+		UIView *grayView = [[UIView alloc] initWithFrame:frame];
+		grayView.backgroundColor = [DTColor blackColor];
+		
+		// find a player for this URL if we already got one
+		MPMoviePlayerController *player = nil;
+		for (player in self.mediaPlayers)
+		{
+			if ([player.contentURL isEqual:url])
+			{
+				break;
+			}
+		}
+		
+		if (!player)
+		{
+			player = [[MPMoviePlayerController alloc] initWithContentURL:url];
+			[self.mediaPlayers addObject:player];
+		}
+		
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_4_2
+		NSString *airplayAttr = [attachment.attributes objectForKey:@"x-webkit-airplay"];
+		if ([airplayAttr isEqualToString:@"allow"])
+		{
+			if ([player respondsToSelector:@selector(setAllowsAirPlay:)])
+			{
+				player.allowsAirPlay = YES;
+			}
+		}
+#endif
+		
+		NSString *controlsAttr = [attachment.attributes objectForKey:@"controls"];
+		if (controlsAttr)
+		{
+			player.controlStyle = MPMovieControlStyleEmbedded;
+		}
+		else
+		{
+			player.controlStyle = MPMovieControlStyleNone;
+		}
+		
+		NSString *loopAttr = [attachment.attributes objectForKey:@"loop"];
+		if (loopAttr)
+		{
+			player.repeatMode = MPMovieRepeatModeOne;
+		}
+		else
+		{
+			player.repeatMode = MPMovieRepeatModeNone;
+		}
+		
+		NSString *autoplayAttr = [attachment.attributes objectForKey:@"autoplay"];
+		if (autoplayAttr)
+		{
+			player.shouldAutoplay = YES;
+		}
+		else
+		{
+			player.shouldAutoplay = NO;
+		}
+		
+		[player prepareToPlay];
+		
+		player.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+		player.view.frame = grayView.bounds;
+		[grayView addSubview:player.view];
+		
+		return grayView;
+	}
+	else if (attachment.contentType == DTTextAttachmentTypeImage)
+	{
+		// if the attachment has a hyperlinkURL then this is currently ignored
+		DTLazyImageView *imageView = [[DTLazyImageView alloc] initWithFrame:frame];
+		imageView.delegate = self;
+		if (attachment.contents)
+		{
+			imageView.image = attachment.contents;
+		}
+		
+		// url for deferred loading
+		imageView.url = attachment.contentURL;
+		
+		// if there is a hyperlink then add a link button on top of this image
+		if (attachment.hyperLinkURL)
+		{
+			// NOTE: this is a hack, you probably want to use your own image view and touch handling
+			// also, this treats an image with a hyperlink by itself because we don't have the GUID of the link parts
+			imageView.userInteractionEnabled = YES;
+			
+			DTLinkButton *button = [[DTLinkButton alloc] initWithFrame:imageView.bounds];
+			button.URL = attachment.hyperLinkURL;
+			button.minimumHitSize = CGSizeMake(25, 25); // adjusts it's bounds so that button is always large enough
+			button.GUID = attachment.hyperLinkGUID;
+			
+			// use normal push action for opening URL
+			[button addTarget:self action:@selector(linkPushed:) forControlEvents:UIControlEventTouchUpInside];
+			
+			// demonstrate combination with long press
+			UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(linkLongPressed:)];
+			[button addGestureRecognizer:longPress];
+			
+			[imageView addSubview:button];
+		}
+		
+		return imageView;
+	}
+	else if (attachment.contentType == DTTextAttachmentTypeIframe)
+	{
+		DTWebVideoView *videoView = [[DTWebVideoView alloc] initWithFrame:frame];
+		videoView.attachment = attachment;
+		
+		return videoView;
+	}
+	else if (attachment.contentType == DTTextAttachmentTypeObject)
+	{
+		// somecolorparameter has a HTML color
+		UIColor *someColor = [UIColor colorWithHTMLName:[attachment.attributes objectForKey:@"somecolorparameter"]];
+		
+		UIView *someView = [[UIView alloc] initWithFrame:frame];
+		someView.backgroundColor = someColor;
+		someView.layer.borderWidth = 1;
+		someView.layer.borderColor = [UIColor blackColor].CGColor;
+		
+		return someView;
+	}
+	
+	return nil;
+}
+
+- (BOOL)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView shouldDrawBackgroundForTextBlock:(DTTextBlock *)textBlock frame:(CGRect)frame context:(CGContextRef)context forLayoutFrame:(DTCoreTextLayoutFrame *)layoutFrame
+{
+	UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:CGRectInset(frame,1,1) cornerRadius:10];
+    
+	CGColorRef color = [textBlock.backgroundColor CGColor];
+	if (color)
+	{
+		CGContextSetFillColorWithColor(context, color);
+		CGContextAddPath(context, [roundedRect CGPath]);
+		CGContextFillPath(context);
+		
+		CGContextAddPath(context, [roundedRect CGPath]);
+		CGContextSetRGBStrokeColor(context, 0, 0, 0, 1);
+		CGContextStrokePath(context);
+		return NO;
+	}
+	
+	return YES; // draw standard background
+}
+
+
+#pragma mark Actions
+
+- (void)linkPushed:(DTLinkButton *)button
+{
+	
     NSDictionary * dict = [NSDictionary dictionaryWithObjectsAndKeys:
                            button.GUID, @"link_id",
                            [button.URL absoluteString], @"url",
@@ -134,33 +331,21 @@
     [self.proxy fireEvent:@"click" withObject:dict];
 }
 
-- (UIView *)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView viewForLink:(NSURL *)url identifier:(NSString *)identifier frame:(CGRect)frame {
-    
-	DTLinkButton *button = [[DTLinkButton alloc] initWithFrame:frame];
-	button.URL = url;
-	button.minimumHitSize = CGSizeMake(25, 25); // adjusts it's bounds so that button is always large enough
-	button.GUID = identifier;
-    
-	// use normal push action for opening URL
-	[button addTarget:self action:@selector(linkPushed:) forControlEvents:UIControlEventTouchUpInside];
-    
-	return button;
+- (void)linkLongPressed:(UILongPressGestureRecognizer *)gesture
+{
+	if (gesture.state == UIGestureRecognizerStateBegan)
+	{
+		DTLinkButton *button = (id)[gesture view];
+		button.highlighted = NO;
+		
+        NSDictionary * dict = [NSDictionary dictionaryWithObjectsAndKeys:
+                               button.GUID, @"link_id",
+                               [button.URL absoluteString], @"url",
+                               nil];
+        [self.proxy fireEvent:@"longpress" withObject:dict];
+	}
 }
 
-- (UIView *)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView viewForAttachment:(DTTextAttachment *)attachment frame:(CGRect)frame {
-    if (attachment.contentType == DTTextAttachmentTypeImage) {
-        DTLazyImageView * imageView = [[DTLazyImageView alloc] initWithFrame:frame];
-        imageView.delegate = self;
-        if (attachment.contents) {
-            imageView.image = attachment.contents;
-        }
-        imageView.url = attachment.contentURL;
-        // TODO linked image?
-        return imageView;
-    }
-    
-    return nil;
-}
 
 #pragma mark DTLazyImageViewDelegate
 
@@ -171,18 +356,36 @@
 	NSPredicate *pred = [NSPredicate predicateWithFormat:@"contentURL == %@", url];
 	
 	// update all attachments that matchin this URL (possibly multiple images with same size)
-	for (DTTextAttachment *oneAttachment in [view.layoutFrame textAttachmentsWithPredicate:pred]) {
+	for (DTTextAttachment *oneAttachment in [view.layoutFrame textAttachmentsWithPredicate:pred])
+	{
 		oneAttachment.originalSize = imageSize;
 		
-		if (!CGSizeEqualToSize(imageSize, oneAttachment.displaySize)) {
+		if (!CGSizeEqualToSize(imageSize, oneAttachment.displaySize))
+		{
 			oneAttachment.displaySize = imageSize;
 		}
 	}
 	
-	// redo layout
-	// here we're layouting the entire string, might be more efficient to only relayout the paragraphs that contain these attachments
+	// layout might have changed due to image sizes
     [(TiViewProxy *)[self proxy] contentsWillChange];
 	[view relayoutText];
 }
+
+#pragma mark Properties
+
+- (NSMutableSet *)mediaPlayers
+{
+	if (!mediaPlayers) {
+		mediaPlayers = [[NSMutableSet alloc] init];
+	}
+	
+	return mediaPlayers;
+}
+
+@synthesize fileName = _fileName;
+@synthesize mediaPlayers;
+@synthesize baseURL;
+
+
 
 @end
--- mobile/ios/example/app.js
+++ mobile/ios/example/app.js
@@ -1,44 +1,70 @@
-
-
 var RichText = require('com.obscure.RichText');
 
-var win = Ti.UI.createWindow({ backgroundColor:'#ccc', layout: 'vertical' });
-
-var font = {
-  fontFamily: 'AmericanTypewriter',
-  fontSize: 14,
-};
-
-win.add(Ti.UI.createLabel({
-  height: 24,
-  backgroundColor: '#fcc',
-  font: font,
-  text: "reference font"
-}));
-
-var view = RichText.createRichTextView({                                       
-  width: 200,
-  height: Ti.UI.SIZE,
-  font: font,
-  html: "<h2>Important message!</h2><p>hello, <b>world</b>! I'm <i>really</i> happy to see you.</p>",
+var win = Ti.UI.createWindow({
+	backgroundColor : '#ccc',
+	layout : 'vertical'
 });
-win.add(view);
-
-var bgview = RichText.createRichTextView({
-   width: 200,
-   height: Ti.UI.SIZE,
-   color: 'yellow',
-   backgroundColor: 'blue',
-   font: {
-     fontFamily: 'Copperplate',
-     fontSize: 36
-   },
-   html: "<p>para 1</p><p>para 2</p>",
+var testData = [ 
+	{title: 'About this module', file:"About.html"}, 
+	{title: 'READ ME', file:"README.html"}, 
+	{title: 'iOS6', file:"iOS6.html"}, 
+	{title: 'CSS Styles', file:"styles.html"},
+	{title: 'Line Height', file:"LineHeight.html"}, 
+	{title: 'Text Alignment', file:"Alignment.html"}, 
+	{title: 'Image Handling', file:"Image.html"}, 
+	{title: 'Video', file:"Video.html"}, 
+	{title: 'Space and Newline Handling', file:"APOD.html"}, 
+	{title: 'Extremely long HTML document', file:"WarAndPeace.html"}, 
+	{title: 'Custom Fonts', file:"CustomFont.html"}, 
+	{title: 'Custom Subviews', file:"Subviews.html"}, 
+	{title: 'Nested Lists', file:"ListTest.html"}, 
+	{title: 'Arabic Text', file:"ArabicTest.html"}, 
+	{title: 'Multiple Languages Support', file:"Languages.html"}, 
+	{title: 'Text Boxes', file:"TextBoxes.html"}, 
+	{title: 'Emoji Test', file:"EmojiTest.html"}, 
+	{title: 'Known CoreText Issues', file:"CoreTextIssues.html"}, 
+	{title: 'Font Sizes', file:"FontSizes.html"}
+];
+var table = Ti.UI.createTableView({
+	data: testData
 });
-win.add(bgview);
+table.addEventListener("click", openWin);
+win.add(table);
 
-win.open();
 
-setTimeout(function() {
-  view.html = "<h1>bye bye!</h1>";
-}, 5000);
+function openWin(e) {
+	var content = readFile(e.row.file);
+	
+	var win = Ti.UI.createWindow({backgroundColor : '#FFF'});
+	var scrollView = Ti.UI.createScrollView({
+	    left:0,right:0,top:0,
+	    contentHeight:'auto',
+	    contentWidth:"100%",
+	    showVerticalScrollIndicator: true,
+	    showHorizontalScrollIndicator: false
+	});
+	var richTextView = RichText.createRichTextView({ // <-- this is the module view
+		top:10,
+		left:10,
+		right:10,
+		height : Ti.UI.SIZE,
+		html : content
+	});
+	richTextView.addEventListener("click", function(e){
+		Ti.API.info(e);
+	});
+	scrollView.add(richTextView);
+	win.add(scrollView);
+	navGroup.open(win);
+}
+
+function readFile(filename){
+	var html = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, filename);
+	return html.read().text;
+}
+
+//navgroup
+var window = Ti.UI.createWindow();
+var navGroup = Ti.UI.iPhone.createNavigationGroup({ window : win });
+window.add(navGroup);
+window.open();
